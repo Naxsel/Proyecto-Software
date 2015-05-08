051d1cd8636ed06dfba67e6cd33cfb64744c7f24
AdrianAlcolea <adrianalcoleamoreno@gmail.com>
2015-05-09 01:12:39 +0200

    "Cliente con ordenacion"

--- pasos_git.sh
+++ pasos_git.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
+echo "Intro para realizar add"
+read i
 git add .
-git commit -m "$1"
+echo "add realizado"
+echo "Escribir comentario e Intro para realizar commit"
+read coment
+git commit -m "$coment"
+echo "commit realizado"
+echo "Intro para realizar pull"
+read i
 git pull origin master
+echo "pull realizado"
+echo "Intro para realizar add"
+read i
+git add .
+echo "add realizado"
+echo "Intro para realizar commit (mismo comentario)"
+read i
+git commit -m "$coment"
+echo "commit realizado"
+echo "Intro para realizar push"
+read i
 git push origin master
+echo "push realizado"
+read i
