d9f23fc6426626ee721ff507444e2fdd600b0c37
agustin <agusnavarro11@gmail.com>
2015-05-11 16:55:13 +0200

    Compilación con java 1.7

--- compile.sh
+++ compile.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
+rm -rf ./bin
 mkdir ./bin
 
 #javac -d ./bin/servidor -cp ".:/lib/*.jar" ./src/servidor/servidorchat/*.java
 # Util
-javac -d ./bin ./src/util/*.java
+javac -source 1.7 -target 1.7 -d ./bin ./src/util/*.java
 echo "Compilado ./src/util"
 # Cliente
-javac -d ./bin -cp ./bin ./src/cliente/*.java
+javac -source 1.7 -target 1.7 -d ./bin -cp ./bin ./src/cliente/*.java
 echo "Compilado ./src/cliente"
 # Admin
-javac -d ./bin -cp ./bin ./src/admin/*.java
+javac -source 1.7 -target 1.7 -d ./bin -cp ./bin ./src/admin/*.java
 echo "Compilado ./src/admin"
 # Servidor
-javac -d ./bin -cp "./lib/*.jar:./bin" ./src/servidor/*.java
+javac -source 1.7 -target 1.7 -d ./bin -cp "./lib/*.jar:./bin" ./src/servidor/*.java
 echo "Compilado ./src/servidor"
